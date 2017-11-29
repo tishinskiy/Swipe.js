656df8564583acaf71e9f2a6b6baac1055cfa7aa
Tishinskiy <tishinskiy@mail.ru>
2017-11-29 12:21:20 +0700

    corrected iphone 5 action

--- src/swipe.js
+++ src/swipe.js
@@ -8,14 +8,16 @@
 		var touch = false;
 		var posX = 0;
 		var move = block.find('.swipe--content');
-		var tl;
+		var tl = 1;
 		var mw = 0;
+		move.css({left: 0})
 
 		var blockMove = function(e){
-			ml = tl + (-(posX - e))
+			if (tl == NaN) {tl = Number(0)}
+			var ml = tl + (-(posX - e))
 			if (ml > 0) {ml = 0}
 			if ((block.innerWidth() - ml) > mw) {ml = -( mw - block.innerWidth())}
-			move.css({left: ml});
+			move.css({left: ml, position: 'relative'});
 		}
 
 		block.bind(
@@ -31,6 +33,7 @@
 					posX = e.originalEvent.touches[0].pageX;
 					tl = parseInt(move.css('left'));
 					mw = move[0].scrollWidth;
+					// alert(11111)
 				},
 				mouseup: function(e){
 					touch = false;
@@ -47,8 +50,11 @@
 					}
 				},
 				touchmove: function(e){
+					e.preventDefault();
+					var t = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
 					if(touch == true) {
-						blockMove(e.originalEvent.touches[0].pageX);
+						blockMove(t.pageX);
+						// alert("move "+t.pageX);
 					}
 				}
 			}
